5fae02b192121e2feb22ff65249095267a41f9f0
Aman Thakral <a@thakral.in>
2019-08-01 06:14:05 -0700

    Understanding await

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace something
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var t = new TestAsync();
+            t.FirstAsync();
+            await t.SecondAsync();
+            return 0;
+        }
+    }
+
+    class TestAsync {
+        public async Task FirstAsync() {
+            await File.WriteAllTextAsync("First.txt",$"First Run {DateTime.UtcNow}");
+            await Task.Delay(100000);
+        }
+
+
+        public async Task SecondAsync() {
+            await File.WriteAllTextAsync("Second.txt",$"Second Run {DateTime.UtcNow}");
+            await Task.Delay(10);
+        }
+    }
+}
